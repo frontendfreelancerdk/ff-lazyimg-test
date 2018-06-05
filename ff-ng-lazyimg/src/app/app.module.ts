4ed14f35a38f9016a3391e7ccc9243fe4726293e
Jakob Nielsen <web@jakobhnielsen.com>
2018-06-05 21:20:14 +0200

    .mod implement lib-ng-ff-lazyimg - name will change dont worry

--- ff-ng-lazyimg/src/app/app.module.ts
+++ ff-ng-lazyimg/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NgFfLazyimgModule } from "ng-ff-lazyimg";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgFfLazyimgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
